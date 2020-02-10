21c146a936686f34f44b8e9745cd46dbfae475dd
Anonymac69 <57042937+Anonymac69@users.noreply.github.com>
2020-02-10 21:10:18 +0100

    changed database, dbsqlite to postgresql

--- bluefinch/settings.py
+++ bluefinch/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bluefinch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_dreamgraphix',
+        'USER': 'postgres',
+        'PASSWORD': '6Kandahar9',
+        'HOST': 'localhost',
+        'PORT': '5000'
     }
 }
 
