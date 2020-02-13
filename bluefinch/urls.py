db49679c7f0f27e77deccac9c85b7c6868c94342
Anonymac69 <57042937+Anonymac69@users.noreply.github.com>
2020-02-13 11:10:49 +0100

    modified templates & staticfiles

--- bluefinch/urls.py
+++ bluefinch/urls.py
@@ -23,4 +23,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('surface.urls')),
     path('', include('authenticate.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
