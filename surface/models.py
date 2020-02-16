82ef2725178d791f4c8650901cf6d5ab628a1953
Anonymac69 <57042937+Anonymac69@users.noreply.github.com>
2020-02-16 05:12:09 +0100

    bootstrap styling

--- surface/models.py
+++ surface/models.py
@@ -5,3 +5,6 @@ class Pane(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=30)
     summary = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
