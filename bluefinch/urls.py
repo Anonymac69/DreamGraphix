141c04e6f44ecfe7217bad9b6a1f182257c19e06
Anonymac69 <57042937+Anonymac69@users.noreply.github.com>
2020-02-18 11:53:46 +0100

    mod settings, urls + index

--- bluefinch/urls.py
+++ bluefinch/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('surface.urls')),
     path('account/', include('authenticate.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 if settings.DEBUG:
